f2e12d8f60a225f94f44b44cd48d0ac451fcf271
ryota.sego <sego@lassic.co.jp>
2021-04-16 06:24:37 +0000

    post

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy("id", "desc")->paginate(10);
+        
+        return view("users.index", ['users' => $users]);
+        
+    }
+    
+    public function show($id){
+        
+        $user = User::findOrFail($id);
+        
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
+        return view('users.show', [
+            'user' => $user,
+            'microposts' => $microposts,
+        ]);
+    }
+}
