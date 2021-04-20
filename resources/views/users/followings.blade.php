b8ee0ae0b0ec0b89425c4fa8f38bfb66c607fb71
ryota.sego <sego@lassic.co.jp>
2021-04-20 02:28:21 +0000

    follow, unfollow

---
+++ resources/views/users/followings.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts,app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            @incrude('users.card')
+        </aside>
+        <div class="col-sm-8">
+            {{-- タブ --}}
+            @include('users.navtabs')
+            {{-- ユーザ一覧 --}}
+            @include('users.users')
+        </div>
+    </div>
+@endsection
